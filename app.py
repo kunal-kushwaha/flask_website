8397ce29d780e5fe966eeaa0c70953f377d79910
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-01-06 22:41:24 +0530

    New Website

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from article import Article
 app = Flask(__name__)
 
-posts = [
-    Article('title', 'subtitle', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 'jatin katyal'),
-    Article('title', 'subtitle', 'body', 'kunal kushwaha'),
-    Article('title', 'subtitle', 'body', 'shashank'),
-    Article('title', 'subtitle', 'body', 'apoorv goyal')
-]
+posts = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', size=len(posts))
 
 @app.route('/articles')
 def articles():
@@ -24,6 +19,29 @@ def article(id):
         return render_template('article.html', article=post)
     except IndexError:
         return render_template('404.html')
+
+@app.route('/new_article', methods=['GET', 'POST'])
+def new_article():
+    if request.method == "GET":
+        return render_template('new_article.html')
+    else:
+        article = Article(
+            request.form['title'],
+            request.form['subtitle'],
+            request.form['body'],
+            request.form['author']
+        )
+        posts.append(article)
+        return "Created Successfully"
+
+@app.route('/tv')
+def tv():
+    return render_template('tv.html')
+
+@app.route('/songs')
+def songs():
+    return render_template('songs.html')
+
 
 
 if __name__ == '__main__':
