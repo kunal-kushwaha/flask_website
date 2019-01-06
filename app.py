002adc5c7baa91c4d8ce474025eb33e52edb4f11
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-01-07 00:55:04 +0530

    third commit

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template, request
 from article import Article
 app = Flask(__name__)
 
-posts = []
+postss = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', size=len(posts))
+    return render_template('index.html', size=len(postss))
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles=posts)
+    return render_template('articles.html', articles=postss)
 
 @app.route('/articles/<int:id>')
 def article(id):
     try: 
-        post = posts[id-1]
+        post = postss[id-1]
         return render_template('article.html', article=post)
     except IndexError:
         return render_template('404.html')
